fa10ec19e1593e619cb56f34241a868eff803774
theodorsjetnanutvik <theodor.utvik@gmail.com>
2026-04-03 00:27:59 +0200

    Remove duplicate Pages list from sidebar

--- app/components/sidebar.py
+++ app/components/sidebar.py
@@ -6,22 +6,6 @@ def render_sidebar():
         st.divider()
         st.markdown(
             """
-            <div style='font-size: 0.78rem; color: #888; line-height: 1.7'>
-                <b style='color: #ccc'>Pages</b><br>
-                — &nbsp;ELO Rankings<br>
-                — &nbsp;Match Predictor<br>
-                — &nbsp;Player Profile<br>
-                — &nbsp;Tournament History<br>
-                — &nbsp;Head to Head<br>
-                — &nbsp;ELO Race<br>
-                — &nbsp;Pair Chemistry
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
-        st.divider()
-        st.markdown(
-            """
             <div style='font-size: 0.75rem; color: #666; line-height: 1.8'>
                 <b style='color: #999'>About</b><br>
                 Data collected from padelapi.org<br>
